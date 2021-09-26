5b90d1c663cdab9310689f8339f976d2a8936774
Markus Westerlind <marwes91@gmail.com>
2021-09-26 21:19:30 +0200

    feat: Map the directional buttons onto the joystick

--- main.py
+++ main.py
@@ -31,30 +31,64 @@ for button in buttons:
 gamepad = Gamepad(usb_hid.devices)
 
 gamepad_buttons = [
-    # Unused
-    1, 2,
-    # Movement
-    3, 4, 5,
-    # Jump
-    6,
-    # Options
-    7, 8,
-    # Attack buttons
-    9, 10, 11, 12, 13, 14,
     # Extra
+    1, 2,
+    # Attack buttons
+    3, 4, 5, 6, 7, 8,
+    # Options
+    9, 10,
+    # Movement
+    11, 12, 13, 14,
+    # Unused
     15, 16
 ]
 
 button_states = [False for x in gamepad_buttons]
 
+
+def is_movement(gamepad_button_num):
+    return 11 <= gamepad_button_num <= 14
+
+
 while True:
     # Buttons are grounded when pressed (.value = False).
     for i, button in enumerate(buttons):
         gamepad_button_num = gamepad_buttons[i]
+        if is_movement(gamepad_button_num):
+            continue
         if button.value and button_states[i]:
             button_states[i] = False
             gamepad.release_buttons(gamepad_button_num)
         elif not button.value and not button_states[i]:
             button_states[i] = True
             gamepad.press_buttons(gamepad_button_num)
+
+    LEFT = 13
+    RIGHT = 11
+    UP = 10
+    DOWN = 12
+    x = None
+    y = None
+    # SOCD cleaner
+    # print(not buttons[LEFT].value, not buttons[RIGHT].value,
+    #      not buttons[UP].value, not buttons[DOWN].value)
+    if not buttons[LEFT].value:
+        if not buttons[RIGHT].value:
+            x = 0
+        else:
+            x = -127
+    elif not buttons[RIGHT].value:
+        x = 127
+    else:
+        x = 0
+
+    if not buttons[UP].value:
+        y = -127
+    elif not buttons[DOWN].value:
+        y = 127
+    else:
+        y = 0
+
+    gamepad.move_joysticks(x, y)
+
     time.sleep(0.001)
